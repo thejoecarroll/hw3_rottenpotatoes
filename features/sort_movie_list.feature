bbd4273d67f4740aadc8192b19affe5530f5b0da
Joe Carroll <joe.carroll@gmail.com>
2012-10-28 20:56:00 +0200

    Added some actual test steps for sorting.

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -24,8 +24,12 @@ Background: movies have been added to database
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
   # your steps here
+  Then I should see "Aladdin" before "Amelie"
+  And I should see "2001: A Space Odyssey" before "When Harry Met Sally"
+
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
   # your steps here
+  Then I should see "The Terminator" before "The Help"
 
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.index(e1) < page.body.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +25,5 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+
 end
